f37c56545df130e82b18f0c2cdd0af2d314acac6
Ricardo Matos <ricardomatos_976@hotmail.com>
2024-08-11 18:01:50 +0000

    created js script that changes navbar colout from transpareny on load to coloured

--- js/script.js
+++ js/script.js
@@ -13,3 +13,13 @@ $(document).ready(function () {
   }
  
 });
+
+let headerEl = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+if (window.scrollY > 50) {
+  headerEl.classList.add("header-scroll")
+}
+else if (window.scrollY <=50 ) {
+  headerEl.classList.remove("header-scroll")
+}
+});
