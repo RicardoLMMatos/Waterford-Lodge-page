1c39f13635441ece96eaf1505aa95e76596d098f
Ricardo Matos <ricardomatos_976@hotmail.com>
2024-08-16 14:07:42 +0000

    changed dropdown menu do change colour on scroll

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   let $reviewsSlick = $('.function-photos-slick');
   if ($reviewsSlick.length > 0) {
     $reviewsSlick.slick({
-      'infinite' : true,
+      'infinite': true,
       'arrows': false,
       'dots': false,
       'autoplay': true,
@@ -13,32 +13,29 @@ $(document).ready(function () {
       'adaptiveHeight': false
     });
   }
- 
+
 });
 
 // js code for nav bar colour change on scroll
 
 let headerEl = document.querySelector("header");
 window.addEventListener("scroll", () => {
-if (window.scrollY > 50) {
-  headerEl.classList.add("header-scroll")
-}
-else if (window.scrollY <=50 ) {
-  headerEl.classList.remove("header-scroll")
-}
+  if (window.scrollY > 50) {
+    headerEl.classList.add("header-scroll")
+  } else if (window.scrollY <= 50) {
+    headerEl.classList.remove("header-scroll")
+  }
 
 
 });
 
- // Javascript for the burger menu 
-      
-     
-        function myFunction() {
-            var x = document.getElementById("burger-menu");
-            if (x.style.display === "flex") {
-                x.style.display = "none";
-            } else {
-                x.style.display = "flex";
-            }
-        }
-    
+// Javascript for the burger menu 
+
+function myFunction() {
+  var x = document.getElementById("burger-menu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
